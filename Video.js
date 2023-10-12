69ba1253ea295391c5fbb006c154dc7d69cb1d88
Serge Turcheniuk <sergio.turcheniuk@sky.co.nz>
2023-10-13 09:34:57 +1300

    comment out error

--- Video.js
+++ Video.js
@@ -310,11 +310,11 @@ export default class Video extends Component {
     const isNetwork = !!(uri && uri.match(/^https?:/i));
     const isAsset = !!(uri && uri.match(/^(assets-library|ph|ipod-library|file|content|ms-appx|ms-appdata):/i));
 
-    if ((uri || uri === '') && !isNetwork && !isAsset) {
-      if (this.props.onError) {
-        this.props.onError({error: {errorString: 'invalid url, player will stop', errorCode: 'INVALID_URL'}});
-      }
-    }
+    // if ((uri || uri === '') && !isNetwork && !isAsset) {
+    //   if (this.props.onError) {
+    //     this.props.onError({error: {errorString: 'invalid url, player will stop', errorCode: 'INVALID_URL'}});
+    //   }
+    // }
 
     let nativeResizeMode;
     const RCTVideoInstance = this.getViewManagerConfig('RCTVideo');
