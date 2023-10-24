44f360bd321dd656cbf6c3cd3da7d9b1d5329605
Serge Turcheniuk <sergio.turcheniuk@sky.co.nz>
2023-10-24 17:49:57 +1300

    added a check for player

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -253,7 +253,9 @@ class ReactExoplayerView extends FrameLayout implements
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-        initializePlayer();
+        if(player == null){
+                initializePlayer();
+        }
     }
 
     @Override
